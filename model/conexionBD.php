be7e7b07df862e63bc08ff8b3e4eb36aeef08f13
Brando Valenzuela <brando.jpvalenzuela@gmail.com>
2024-12-16 08:36:34 -0600

    Funcion: mostrat alumnos. Ya es posible mostrar los alumnos inscritos en una clase.

--- model/conexionBD.php
+++ model/conexionBD.php
@@ -5,7 +5,7 @@ require_once 'data.php';
 class Conexion
 {
     public static function obtenerConexion(){
-        $pdo = new PDO('mysql:host=HOST;dbname=DBNAME;charset=CODEX', USER, PASSWORD);
+        $pdo = new PDO('mysql:host='.HOST.';dbname='.DBNAME.';charset='.CODEX.'', USER, PASSWORD);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
